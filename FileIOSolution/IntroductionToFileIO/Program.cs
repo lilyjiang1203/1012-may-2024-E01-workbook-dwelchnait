78ffc06b50e3e58e151cf86e36f59cfc1374152a
Don Welch <dwelch@nait.ca>
2024-07-05 18:53:17 -0600

    Read method

--- FileIOSolution/IntroductionToFileIO/Program.cs
+++ FileIOSolution/IntroductionToFileIO/Program.cs
@@ -43,7 +43,114 @@ WriteNamesandMark(studentNames, studentMarks, logicalSize, FILENAME);
 //Methods
 static int ReadNamesandMarks(string[] studentNames, double[] studentMarks, string fileName)
 {
+    //the skeleton of the method is often referred to as a method stub
+    //method reminder: parameters are "treated" as local variables
+
+    //place a few comments within the method to explain what the method will do
+    //Purpose: Read the course grades file and store the data to the arrays
+
+    //steps:
+    // a) Declare local variables: StreamReader, counter, input string
+    // b) Test to see if the file exists
+    //    1) no: display an appropriate message and exit method
+    //    2) yes: read the file
+    //            process each file record on at a time
+    //            for each record, split data and store into arrays
+    //            increment record count for each record read
+
     int countOfRecordsRead = 0;
+    string inputLine = "";
+
+    //there are a couple of techniques to use to read a file
+    //StreamReader (this course uses the StreamReader/StreamWriter technique)
+    //  is useful if you have a very large file and your intention
+    //      is that you may not be reading the entire file
+    //      OR
+    //      the file will not total fit in your machine
+    //use the System.IO.File class
+    //  is class have serval methods create that allow for
+    //      reading of files that are not extremely large
+    //      AND your intent is to read the entire file
+
+    //Need to know
+    //a) where is the file located on your machine
+    //b) what is the file name
+
+    //by default 
+    //  your program will expect the physical to be in the same folder as the .exe file when you compile
+
+    //you can use relative addressing to indicate a different location then the default
+    //you can add several ../ folder increments to reposition the location to find your physical file
+
+    string filePath = $"../../../{fileName}";
+
+    //create an instance variable of the reader that will manager the File Input
+    StreamReader reader = null;
+
+    //use the File class to test if a file exists BEFORE trying to use
+    if (File.Exists(filePath))
+    {
+        //read the file
+        //as you read your file, if an error occurs you need to catch the error and handle it
+        try
+        {
+            //setup your instance for the StreamReader
+            reader = new StreamReader(filePath);
+
+            //read the file record by record (loop)
+            //when you reach the end of the file, the system will set a flag called EndOfStream
+            //   this will indicate that you have reached the end of the file
+            while(!reader.EndOfStream)
+            {
+                //read a record
+                inputLine = reader.ReadLine();
+
+                //next, we need to separate the values on the line into
+                //  their appropriate arrays
+                //to do this, we will use a string method call .Split('deliminator')
+                //the deliminator can be any single character.
+                //for a csv file (comma separate values) the deliminator is a comma ,
+                //the Split() method will split the data on the line into separate values
+                //  at the deliminator
+                //the Split() method returns the values of the line to an string array
+                string[] lineValues = inputLine.Split(',');
+
+                //note: assuming that the data in the file is valid
+
+                studentNames[countOfRecordsRead] = lineValues[0];
+                studentMarks[countOfRecordsRead] = double.Parse(lineValues[1]);
+
+                //increment count of records read
+                countOfRecordsRead++;
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Read Error: {ex.Message}");
+        }
+        finally
+        {
+            //this is an optionally piece of code that can be used on your try/catch
+            //normally
+            //  you will execute all of the try successfully, exit the try/catch
+            //or
+            //  an error will be generated and you will process the catch, exit the try/catch
+
+            //the finally will be execute WHETHER you exited the try/catch successful or having
+            //      an error
+
+            //in this code, we will manually close the file that is being read
+            if (reader != null) //try to close ONLY if you have a reader
+            {
+                reader.Close(); //manually close the file
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"\n\t Your file {fileName} does not exists. No records read.");
+    }
+
     return countOfRecordsRead;
 }
 
