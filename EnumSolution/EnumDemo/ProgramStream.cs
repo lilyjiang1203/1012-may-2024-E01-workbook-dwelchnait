29d37331da61fdd462f7764b1424a77759192aa6
Don Welch <dwelch@nait.ca>
2024-07-22 18:24:31 -0600

    Enum demo

---
+++ EnumSolution/EnumDemo/ProgramStream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    public enum ProgramStream
+    {
+        CSD,
+        BA,
+        SA,
+        VIS,
+        Gaming,
+        VM
+    }
+}
