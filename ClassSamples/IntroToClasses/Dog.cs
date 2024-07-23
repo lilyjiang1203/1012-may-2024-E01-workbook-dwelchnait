2319e18a50157c9417ee1db87ab31a9bfc62fbdb
Don Welch <dwelch@nait.ca>
2024-07-22 18:54:18 -0600

    Intro to classes

---
+++ ClassSamples/IntroToClasses/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClasses
+{
+    public class Dog
+    {
+        //declare the characteristics of a Pet
+        //the characteristics are variables that will carry a data value for the instance
+        //  of the dog
+        //note the a class can have various datatypes within its definition as long
+        //  as it is a valid C# datatype including any programmer-defined datatype
+
+        //these variables are referred to as data members
+
+        //by default all items in a class have an access type of private
+        //if you wish outside users (any code outside of Dog) to have access to
+        //  items declared within the class definition, you MUST make them public
+        public string name;
+        public int age;
+        public Breed breed;
+    }
+}
---
+++ ClassSamples/IntroToClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IntroToClasses;
+
+Console.WriteLine("\n\tHello, World of Classes\n");
+
+//declare a variable capable of holding an instance (occurance) of the datatype Dog
+//default values for objects is null
+Dog myPet = null;
+
+//to reference an item within your instance, you will use the . (dot) operator
+//if your variable does not contain an instance of the class, it will abort your program
+
+//obtain an instance of your class
+//this is called a class instance
+//use the new operator to obtain an instance of the specified class
+//the result of the new execution is getting a physical memory allocated instance of your class
+myPet = new Dog();
+
+myPet.name = "No";
+myPet.age = 4;
+myPet.breed = Breed.Poodle;
+
+Console.WriteLine($"My pet characteristics are; Name: {myPet.name} Age: {myPet.age} Breed: {myPet.breed}");
+
+
+
